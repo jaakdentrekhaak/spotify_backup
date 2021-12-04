08ca814cf38f5e39ae3f558f1bf666e36fcd3461
Jens Putzeys <jens.putzeys@student.kuleuven.be>
2021-12-04 16:54:05 +0100

    implement getUserProfile and getUserPlaylists

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask, redirect, url_for, render_template, request
 import json
+import requests
 
 app = Flask(__name__)
 logged_in = False
@@ -18,6 +19,11 @@ def home():
         return redirect(url_for('login'))
     else:
         print('[SERVER] Login succeeded')
+        user_profile = getUserProfile()
+        user_name = user_profile['display_name']
+        user_id = user_profile['id']
+        user_playlists = getUserPlaylists()
+        # TODO: get playlist name, href or id and then get all tracks from this playlist
         return render_template('index.html')
 
 
@@ -31,6 +37,35 @@ def getClientId():
     with open('config.json', 'r') as config_file:
         config = json.load(config_file)
         return config['spotify']['client_id']
+
+
+def getUserProfile():
+    return json.loads(requests.get('https://api.spotify.com/v1/me', headers=createHeaders()).content)
+
+
+def getUserPlaylists():
+    items = []
+
+    playlist_response = requests.get(
+        'https://api.spotify.com/v1/me/playlists', headers=createHeaders())
+    response_json = json.loads(playlist_response.content)
+    items.extend(response_json['items'])
+
+    while response_json['next'] is not None:
+        playlist_response = requests.get(
+            response_json['next'], headers=createHeaders())
+        response_json = json.loads(playlist_response.content)
+        items.extend(response_json['items'])
+
+    return items
+
+
+def createHeaders():
+    global access_token
+    return {
+        'Content-Type': 'application/json',
+        'Authorization': f'Bearer {access_token}'
+    }
 
 
 if __name__ == '__main__':
--- static/js/index.js
+++ static/js/index.js
@@ -1,21 +1,3 @@
 document.getElementById('button_generate_json-button').addEventListener('click', function () {
-    document.getElementById('error_url').hidden = true;
 
-    const spotifyPlaylistUrl = document.getElementById('input_spotify_playlist_url');
-
-    if (!spotifyPlaylistUrl.contains('https://open.spotify.com/playlist/') || !spotifyPlaylistUrl.contains('?')) {
-        document.getElementById('error_url').hidden = false;
-        return;
-    }
-
-    var jsonToSend = {
-        'playlist_url': spotifyPlaylistUrl
-    };
-
-    // Send playlist url to server
-    $.ajax('http://localhost:5000/handle_playlist_url', {
-        data: JSON.stringify(jsonToSend),
-        contentType: 'application/json',
-        type: 'POST'
-    });
 }, false);
